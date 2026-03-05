19443ffd21e2102804b5269c2c514f9143d7748b
Takuro Onoue <kusanaginoturugi@gmail.com>
2026-03-05 11:00:32 +0900

    fix items.csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
-items = (1..100).map do |index|
-  {
-    item_code: format("%04d", index),
-    name: "Sample Item #{format('%03d', index)}",
-    value: (index * 100) + 500
-  }
-end
+# items = (1..100).map do |index|
+#   {
+#     item_code: format("%04d", index),
+#     name: "Sample Item #{format('%03d', index)}",
+#     value: (index * 100) + 500
+#   }
+# end
 
-items.each do |attrs|
-  item = Item.find_or_initialize_by(name: attrs[:name])
-  item.item_code = attrs[:item_code]
-  item.value = attrs[:value]
-  item.save!
-end
+# items.each do |attrs|
+#   item = Item.find_or_initialize_by(name: attrs[:name])
+#   item.item_code = attrs[:item_code]
+#   item.value = attrs[:value]
+#   item.save!
+# end
